5be68373e0e5866683adf5321ed9199433a3e4e5
aldiansyah9395 <aldiansyah93@outlook.com>
2025-05-13 18:51:39 +0700

    Update dashin.js

--- js/dashin.js
+++ js/dashin.js
@@ -3,14 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Konfigurasi Firebase
   const firebaseConfig = {
-    apiKey: "AIzaSyBYdbo6du0u3ZxT53lFEXpNccPwTu8czN4",
-    authDomain: "incoming-schedule-monitoring.firebaseapp.com",
-    projectId: "incoming-schedule-monitoring",
-    storageBucket: "incoming-schedule-monitoring.firebasestorage.app",
-    messagingSenderId: "460704037681",
-    appId: "1:460704037681:web:311d272b7ca9250f130e10",
-    databaseURL: "https://incoming-schedule-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    apiKey: "AIzaSyDDw17I5NwibE9BXl0YoILPQqoPQfCKH4Q",
+    authDomain: "inbound-d8267.firebaseapp.com",
+    databaseURL: "https://inbound-d8267-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "inbound-d8267",
+    storageBucket: "inbound-d8267.firebasestorage.app",
+    messagingSenderId: "852665126418",
+    appId: "1:852665126418:web:e4f029b83995e29f3052cb"
   };
+
 
   // Inisialisasi Firebase
   firebase.initializeApp(firebaseConfig);
